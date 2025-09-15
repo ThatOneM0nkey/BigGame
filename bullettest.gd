e684719125b5e1ab80d537bd50aad433e528f50f
ThatOneM0nkey <keshavasaba@gmail.com>
2025-09-15 14:32:17 -0600

    tried to make thingy
    not much

---
+++ bullettest.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var y_direction := Input.get_axis("ui_up", "ui_down")
+	if y_direction:
+		velocity.y = y_direction * SPEED
+	else:
+		velocity.y = y_direction * SPEED
+
+	move_and_slide()
