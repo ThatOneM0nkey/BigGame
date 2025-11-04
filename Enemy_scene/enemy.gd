fd6d75db949821a71f8fb7a20d8a3ecd2be4200c
NaXik <jauresse.diekeu@gmail.com>
2025-11-04 14:58:32 -0700

    Healthbar

--- Enemy_scene/enemy.gd
+++ Enemy_scene/enemy.gd
@@ -1,8 +1,25 @@
 extends CharacterBody2D
- 
+
+
+@onready var healthbar = $Healthbar
 @export var speed = 500
 
-func _process(delta):
+
+func _process(delta): #Ends Character Progress after it reaches the end of the path
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)
 	if get_parent().get_progress_ratio() == 1:
 		queue_free()
+	
+
+func _ready():
+	health = 6
+	on_dead = die
+	healthbar.init_health(health)
+	
+
+func _set_heealth(value):
+	super._set_health(value)
+	if health <= 0 && is_alive:
+		_die()
+		
+	healthbar.health = health
--- Enemy_scene/path_spawner.gd
+++ Enemy_scene/path_spawner.gd
@@ -3,6 +3,7 @@ extends Node2D
 @export var path: PackedScene = preload("res://Enemy_scene/Stage_1.tscn")
 
 
-func _on_timer_timeout():
+func _on_timer_timeout(): #Spawns a Character after a given amount of time
+	
 	var tempPath = path.instantiate()
 	add_child(tempPath)
